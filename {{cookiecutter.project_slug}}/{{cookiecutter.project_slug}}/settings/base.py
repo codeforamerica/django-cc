745ffd9f164be8bcb071eea139979ade86aa404f
Mikela Clemmons <glassresistor@gmail.com>
2016-06-23 14:26:55 -0700

    Made it check for apps templates by default.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/base.py
@@ -55,7 +55,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
         ],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
